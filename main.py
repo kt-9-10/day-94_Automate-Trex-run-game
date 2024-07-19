9f4aa2074137093716443b70c4b4374a35ff9ff1
ka28-dec <60833391+ka28-dec@users.noreply.github.com>
2024-07-19 13:43:42 +0900

    Initial Commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import pyautogui
+import numpy as np
+
+
+ def capture_game_screenshot():
+    x, y, width, height = 440, 930, 200, 10
+
+    game_window_element = pyautogui.screenshot(region=(x, y, width, height))
+    return game_window_element
+
+
+def is_obstacle(game_window_element):
+    game_array = np.array(game_window_element.convert('L'))
+    return np.any(game_array == 83)
+
+
+def jump():
+    pyautogui.press('space')
+
+
+while True:
+    game_window = capture_game_screenshot()
+    game_window.save("image.png")
+    if is_obstacle(game_window):
+        jump()
